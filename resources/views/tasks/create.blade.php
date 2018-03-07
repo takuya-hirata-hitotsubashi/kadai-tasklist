4d0aa935246ad992d510dcb9caf25205deab6cba
takuya-hirata-hitotsubashi <ec2-user@ip-172-31-3-238.us-east-2.compute.internal>
2018-03-07 02:27:30 +0000

    CRUD tasks

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>タスク新規作成ページ</h1>
+
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('追加') !!}
+
+    {!! Form::close() !!}
+
+
+@endsection
