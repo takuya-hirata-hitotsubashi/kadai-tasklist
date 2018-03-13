9eb76608ff16190fd000d114337578a98431234e
takuya-hirata-hitotsubashi <ec2-user@ip-172-31-3-238.us-east-2.compute.internal>
2018-03-13 12:30:27 +0000

    user login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,12 @@ Route::get('/', function () {
 
 Route::resource('tasks', 'TasksController');
 
+//ユーザ認証
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
+
+// ログイン認証
+Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
+Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
+Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 
